0e6cdba4455c3222a17d6f44b9751f57f0e74ff6
Jonathan Carranza <jhoncarranza1@outlook.es>
2024-02-03 13:50:49 -0600

    cree el componente loginj.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,9 @@
+import {Loginj} from '../components/Jonathan/Loginj'
 
 export const Login = () => {
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-cover bg-center bg-orange-400">
-      <h1 className="text-amber-700">ahorasi</h1>
-    </div>
+      <Loginj/>
     </>
     
   )
