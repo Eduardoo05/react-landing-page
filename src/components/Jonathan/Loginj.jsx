568c1135d7fc06c97794cd3a9a711fb0c24a0855
Jonathan Carranza <jhoncarranza1@outlook.es>
2024-02-03 13:51:06 -0600

    cree el componente loginj.jsx

---
+++ src/components/Jonathan/Loginj.jsx
@@ -0,0 +1,10 @@
+
+export const Loginj = () => {
+  return (
+    <>
+        <div className="flex items-center justify-center h-screen bg-cover bg-center bg-orange-400">
+            <h1 className="text-amber-700">ahorasi</h1>
+        </div>
+    </>
+  )
+}
