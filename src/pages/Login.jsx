d91515c441ccdde845eceaecb62d66a2d08b9577
Jonathan Carranza <jhoncarranza1@outlook.es>
2024-02-03 13:42:34 -0600

    comenzando con otra logica los datos y creacion de .env

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,12 @@
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <div className="flex items-center justify-center h-screen bg-cover bg-center bg-orange-400">
+      <h1 className="text-amber-700">ahorasi</h1>
+    </div>
+    </>
+    
   )
 }
+
